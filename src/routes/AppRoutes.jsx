06e84437844d20b5051ac053abe734b5319a8279
Msindazwe-IT <dommdlalose155@gmail.com>
2026-08-19 13:43:39 +0200

    updated the routing

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from '../components/auth/Login';
 import Homepage from '../pages/learner/Homepage';
 import Signup from '../components/auth/Signup';
+import ForgotPassword from '../components/auth/ForgotPassword';
+import VerifyOTP from '../components/auth/VerifyOTP';
+import ResetPassword from '../components/auth/ResetPassword';
 
 const AppRoutes = () => {
   return (
@@ -13,6 +16,9 @@ const AppRoutes = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/learner/homepage" element={<Homepage/>} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/verify-otp" element={<VerifyOTP />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
         {/* Now this is where to add more routes here as you build the app. dont forget */}
       </Routes>
     </Router>
